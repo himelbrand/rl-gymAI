bec46f81068e758dbe72b09c94884350b2dbbc9c
himelbrand <himelbrand@gmail.com>
2020-11-28 18:34:04 +0200

    changes to code's flow

--- frozen-lake/main.py
+++ frozen-lake/main.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pprint
 from datetime import datetime
+from collections import defaultdict
 DEBUG = False
 PRIOR = False
 MAP = '8x8'
 check_states = []
-terminating_states = {16:[15,5,7,11,12],64:[63,59,54,52,49,46,42,41,35,29,19]}
+terminating_states = {'4x4':[15,5,7,11,12],'8x8':[63,59,54,52,49,46,42,41,35,29,19]}
 
 def set_prior(value):
     global PRIOR
@@ -44,29 +45,32 @@ def modify_env(env):
 def draw_initial_state(env):
     while True:
         s_init = env.observation_space.sample()
-        if PRIOR and s_init not in terminating_states[env.observation_space.n]:
+        if PRIOR and s_init not in terminating_states[MAP]:
             break
         elif not PRIOR:
             break
     return s_init
 
-def evaluate(env,pi,gamma):
+def evaluate(env,pi,gamma,episodes_num=100):
     if DEBUG:
         print('Running policy evaluation')
     v = np.zeros(env.observation_space.n)
     goal_reached = 0
-    for _ in range(15):
-        s = env.reset(draw_initial_state(env))
+    for _ in range(episodes_num):
+        s = env.reset(0)
         done = False
         steps = 0
         sample = []
+        counts = defaultdict(int)
         while(not done):
             a = np.argmax(pi[s])
+            counts[int(s)] += 1
             s_tag, r, done, _ = env.step(a)
             sample.append((s,a,r))
             goal_reached += int(r>0)
             s = s_tag
             steps += 1
+            
         for t in range(len(sample)):
             s,a,r = sample[t]
             Gt = r
@@ -76,9 +80,11 @@ def evaluate(env,pi,gamma):
                 discount = gamma**e
                 Gt += r_tag*discount
             v[s] += Gt
-    v = v/15
+        for s in counts:
+            v[s] /= counts[s]
+    v = v/episodes_num
     if DEBUG:
-        print(f'Reached goal in {goal_reached}/15 episodes')
+        print(f'Reached goal in {goal_reached}/{episodes_num} episodes')
     return v
     
 def sarsa(env,Q,pi,gamma,Lambda,alpha,states,actions,eps,max_step=5000,episode_max_steps=250,iters=0):
@@ -87,10 +93,11 @@ def sarsa(env,Q,pi,gamma,Lambda,alpha,states,actions,eps,max_step=5000,episode_m
         print(f'Running iteration {iters} of SARSA')
     goal_reached = 0
     episodes = 0
+    epsilon_decay = 0.9999
     while steps < max_step:
         E = np.zeros(Q.shape)
         episodes += 1
-        s = draw_initial_state(env)
+        s = 0
         a = np.random.choice(actions,p=pi[s])
         env.reset(s)
         for _ in range(episode_max_steps):
@@ -107,12 +114,10 @@ def sarsa(env,Q,pi,gamma,Lambda,alpha,states,actions,eps,max_step=5000,episode_m
             s, a = s_tag, a_tag
             steps += 1
             if done or steps >= max_step: break
-        eps_greedy(eps,pi,Q,states,actions)
+        eps *= epsilon_decay
     if DEBUG:
         print(f'Reached goal {goal_reached}/{episodes} in episodes of this iteration')
-        if goal_reached/episodes > 0.9:
-            print(f'\n\n{"$"*50}{goal_reached/episodes}{"$"*50}\n\n')
-    return steps,episodes
+    return steps,episodes,eps
             
 def eps_greedy(eps,pi,q,states,actions):
     for s in states:
@@ -138,13 +143,12 @@ def check_Q_convergence(Q,og_Q,actions):
             return False
     return True
 
-def get_maxsteps(curr):#maybe try and change number of steps between evaluations
-    return 2500
-    # if curr < 20000:
-    #     return 1000
-    # if curr < 60000:
-    #     return 2000
-    # return 3000
+def get_maxsteps(curr):
+    if curr < 250000:
+        return 2500
+    if curr < 500000:
+        return 5000
+    return 10000
    
 
 def learn_policy(env,actions,states,gamma,Lambda,alpha):
@@ -153,44 +157,23 @@ def learn_policy(env,actions,states,gamma,Lambda,alpha):
     #init pi
     pi = np.ones((len(states),len(actions)))
     pi /= len(actions) 
-    x = []
-    y = []
-    z = []
+    x = [0]
+    y = [0]
     total_steps = 0
     total_episodes = 0
     iters = 0
-    last_iter = False
-    while True:
+    epsilon = 1
+    while total_steps < 1e6:
         iters += 1
-        epsilon = 1/iters #maybe should be different epsilon
-        if not last_iter:
-            og_pi = pi.copy()
-            og_Q = Q.copy()
-        steps,episodes = sarsa(env,Q,pi,gamma,Lambda,alpha,states,actions,epsilon,max_step=get_maxsteps(total_steps),episode_max_steps=250,iters=iters)
+        steps,episodes,epsilon = sarsa(env,Q,pi,gamma,Lambda,alpha,states,actions,epsilon,max_step=get_maxsteps(total_steps),episode_max_steps=250,iters=iters)
         v = evaluate(env,pi,gamma)
         total_episodes += episodes
         total_steps += steps
         x.append(total_steps)
-        y.append(v[0])
-        z.append(v.mean())
-        if check_Q_convergence(Q,og_Q,actions) and check_pi_convergence(pi,og_pi):#checking that ordering stayed the same
-            if last_iter:#giving another chance before deciding we got converged
-                break
-            last_iter = True
-        else:
-            last_iter = False
-    
-    #show plot
-    plt.plot(x,y,label='V(0)')
-    plt.plot(x,z,label='mean(V)')
-    plt.xlabel('time steps')
-    plt.ylabel('policy value')
-    plt.legend()
-    plt.title(f'Learning plot for $\\alpha$={alpha} and $\\lambda$={Lambda} with{" " if PRIOR else "out "} prior')
-    # plt.show()
-    plt.savefig(f'out/{MAP}/plot-{Lambda}_a-{alpha}_{datetime.now().strftime("%d-%m_%H:%M:%S")}.png')
-    plt.close()
-    return pi,total_episodes,total_steps
+        y.append(v[0])  
+        if DEBUG:
+            print(f'current step count is: {total_steps} with epsilon={epsilon}')      
+    return {'x':x,'y':y},pi
 
 def human_agent(env):
     a = -1
@@ -230,32 +213,34 @@ def run_simulation(env,policy=None,human=False):
     print(f'Done in {steps} steps')
     env.close()
 
-def main(gamma=0.95,save=True,load=None,human=False):
+def main(gamma=0.95,human=False):
     print(f'Using {MAP} map')
+    values = {}
     env = init_env()
     if human:
         run_simulation(env,human=True)
         return
-    if load is not None:
-        with open(load, 'rb') as f:
-            pi = np.load(f) 
-            run_simulation(env,policy=pi)
-            return
     tmp_env = modify_env(env)
     nA = env.action_space.n
     nS = env.observation_space.n
-    for Lambda in [0.95,0.9,0.7,0.5]:
-        for alpha in [0.02,0.05,0.07]:
-            print(f'Learning policy using lambda={Lambda} and alpha={alpha}')
-            pi,episodes,steps = learn_policy(tmp_env,range(nA),range(nS),gamma,Lambda,alpha)
-            
-            if save:
-                with open(f'out/{MAP}/pi_l-{Lambda}_a-{alpha}_{datetime.now().strftime("%d-%m_%H:%M:%S")}({"prior" if PRIOR else "no_prior"}).npy','wb') as f:
-                    np.save(f,pi)
+    for Lambda in [0.9,0.7]:
+        for alpha in [0.1,0.05]:
+            if DEBUG:
+                print(f'Learning policy using lambda={Lambda} and alpha={alpha}')
+            label = f'$\\alpha={alpha},\\lambda={Lambda}$'
+            xy,pi = learn_policy(tmp_env,range(nA),range(nS),gamma,Lambda,alpha)
+            values[label] = xy
             print(f'Running a single simulation using learned policy with lambda={Lambda} and alpha={alpha}')
             run_simulation(env,policy=pi)
-            print(f'Finished learning policy after {episodes} episodes and {steps} steps')
-
+    plt.figure(figsize=(20,10))
+    plt.title(r'Learning of policy - $V_{init}$ vs. steps')
+    plt.xlabel('Total steps')
+    plt.ylabel(r'$V_{init}$',rotation=90)
+    for label in values:
+        x,y = values[label]['x'],values[label]['y']
+        plt.plot(x,y,label=label)
+    plt.legend()
+    plt.savefig(f'out/plot{MAP}({datetime.strftime(datetime.now(),"%d-%m_%H:%M")}).png')
 if __name__ == "__main__":
     import argparse
     def parse_args():
@@ -266,8 +251,6 @@ if __name__ == "__main__":
         parser.add_argument('-d',dest='debug', action='store_true',help='use this flag to get debug prints')
         parser.add_argument('-4x4',dest='map', action='store_true',help='use this flag to use 4x4 map')
         parser.add_argument('-p',dest='prior', action='store_true',help='use this flag to use information regarding terminating states')
-        parser.add_argument('-save',dest='save', action='store_true',help='use this flag to save pi to file')
-        parser.add_argument('-load',dest='load',metavar='FILE',help='use this flag to load pi from file',default=None)
         args = parser.parse_args()
         if args.gamma > 1 or args.gamma < 0:
             raise argparse.ArgumentTypeError(f'{args.gamma} must be in the interval [0,1].')
@@ -277,4 +260,4 @@ if __name__ == "__main__":
         return args
 
     args = parse_args()
-    main(gamma=args.gamma,save=args.save,load=args.load,human=args.human)
+    main(gamma=args.gamma,human=args.human)
